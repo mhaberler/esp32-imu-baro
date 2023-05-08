ee6abc63426607f5b7673a702f4dc66579fcc1b2
Michael Haberler <git@mah.priv.at>
2023-05-08 12:19:10 +0200

    sanitize platformio.ini

--- include/Version.h
+++ include/Version.h
@@ -3,23 +3,23 @@
 
 // AUTO GENERATED FILE, DO NOT EDIT
 #ifndef VERSION
-#define VERSION "0.1.4"
+#define VERSION "0.1.5"
 #endif
 #ifndef BUILD_TIMESTAMP
-#define BUILD_TIMESTAMP "2023-05-07 23:02:42.855834"
+#define BUILD_TIMESTAMP "2023-05-08 12:13:51.130224"
 #endif
     
-#define PIOENV	"m5stack-core2"
+#define PIOENV	"hilmar-esp-wrover-kit"
 #define PIOPLATFORM	"espressif32"
 #define PROJECT_DIR	"/Users/mah/Ballon/src/BalloonWare/esp32-imu-baro"
 #define BUILD_TYPE	"debug"
-#define BOARD	"m5stack-core2"
+#define BOARD	"esp-wrover-kit"
   
 #define GIT_REPO_PRESENT  1
-#define GIT_REV	"ae21cc3-dirty"
+#define GIT_REV	"rev-0.2-8-gf2ffe62-dirty"
 #define GIT_AUTHOR	"Michael Haberler"
-#define GIT_SUBJECT	"missing submodules"
-#define GIT_BRANCH	"my-app-main"
-#define GIT_COMMIT_DATE	"Sun, 7 May 2023 22:54:31 +0200"
+#define GIT_SUBJECT	"cleaned platformio.ini"
+#define GIT_BRANCH	"Hil_testing-on-rev-0.2"
+#define GIT_COMMIT_DATE	"Mon, 8 May 2023 12:09:58 +0200"
 
     
