69e58bcc10ae0be97fd4e84f8c927cc4391e51af
Michael Haberler <git@mah.priv.at>
2023-06-23 23:27:44 +0200

    fix SD pindefs for CoreS3

--- src/app/platform_defaults.h
+++ src/app/platform_defaults.h
@@ -52,6 +52,10 @@
 #define SPI1_MOSI -1
 #define SPI1_SCK -1
 
+#define SD_CS_PIN          4
+#define SD_CARD_DETECT_PIN -1
+#define SD_SPI_FREQ        25000
+
 #endif
 
 #if defined(PLATFORM_M5STAMPS3)
